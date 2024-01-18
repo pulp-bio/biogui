a660ed19301eb5479020aadab619747b4fe6ec12
amideiandrea <190571@studenti.unimore.it>
2024-01-18 23:09:00 +0100

    Refactoring for ANGELS

---
+++ gui_semg_acquisition/modules/TEMPONet_gap.py
@@ -0,0 +1,309 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+from math import ceil
+
+class TEMPONet_gap(nn.Module):
+    """
+    TEMPONet architecture:
+    Three repeated instances of TemporalConvBlock and ConvBlock organized as follows:
+    - TemporalConvBlock
+    - ConvBlock
+    Two instances of Regressor followed by a final Linear layer with a single neuron.
+    """
+    def __init__(self, dataset_name='PPG_Dalia', dataset_args={}):
+        super(TEMPONet_gap, self).__init__()
+        
+        self.dil = [
+                2, 2, 1,
+                4, 4, 1,
+                8, 8, 1
+                ]
+        self.rf = [
+                5, 5, 5,
+                9, 9, 5,
+                17, 17, 5
+                ]
+        self.ch = [
+            8, 8, 8,
+            16, 16, 16, 
+            32, 32, 32,
+            64, 1
+        ]
+        # 1st instance of two TempConvBlocks and ConvBlock
+        k_tcb00 = [ceil(self.rf[0]/self.dil[0]),1]
+        self.tcb00 = TempConvBlock(
+                ch_in = 2,
+                ch_out = self.ch[0],
+                k_size = k_tcb00,
+                dil = [self.dil[0],1],
+                pad = [((k_tcb00[0]-1)*self.dil[0]+1)//2,0]
+                )
+        k_tcb01 = [ceil(self.rf[1]/self.dil[1]),1]
+        self.tcb01 = TempConvBlock(
+                ch_in = self.ch[0],
+                ch_out = self.ch[1],
+                k_size = k_tcb01,
+                dil = [self.dil[1],1],
+                pad = [((k_tcb01[0]-1)*self.dil[1]+1)//2,0]
+                )
+        k_cb0 = [ceil(self.rf[2]/self.dil[2]),1]
+        self.cb0 = ConvBlock(
+                ch_in = self.ch[1],
+                ch_out = self.ch[2],
+                k_size = k_cb0,
+                strd = [4,1],
+                pad = [((k_cb0[0]-1)*self.dil[2]+1)//2,0],
+                dilation = [self.dil[2],1],
+                strd_avg  = [2,1]
+                )
+        
+        # 2nd instance of two TempConvBlocks and ConvBlock
+        k_tcb10 = [ceil(self.rf[3]/self.dil[3]),1]
+        self.tcb10 = TempConvBlock(
+                ch_in = self.ch[2],
+                ch_out = self.ch[3],
+                k_size = k_tcb10,
+                dil = [self.dil[3],1],
+                pad = [((k_tcb10[0]-1)*self.dil[3]+1)//2,0]
+                )
+        k_tcb11 = [ceil(self.rf[4]/self.dil[4]),1]
+        self.tcb11 = TempConvBlock(
+                ch_in = self.ch[3],
+                ch_out = self.ch[4],
+                k_size = k_tcb11,
+                dil = [self.dil[4],1],
+                pad = [((k_tcb11[0]-1)*self.dil[4]+1)//2,0]
+                )
+        k_cb1 = [ceil(self.rf[5]/self.dil[5]),1]
+        self.cb1 = ConvBlock(
+                ch_in = self.ch[4],
+                ch_out = self.ch[5],
+                k_size = k_cb1,
+                strd = [4,1],
+                pad = [((k_cb1[0]-1)*self.dil[5]+1)//2,0],
+                strd_avg  = [2,1]
+                )
+
+        # 3td instance of TempConvBlock and ConvBlock
+        k_tcb20 = [ceil(self.rf[6]/self.dil[6]),1]
+        self.tcb20 = TempConvBlock(
+                ch_in = self.ch[5],
+                ch_out = self.ch[6],
+                k_size = k_tcb20,
+                dil = [self.dil[6],1],
+                pad = [((k_tcb20[0]-1)*self.dil[6]+1)//2,0]
+                )
+        k_tcb21 = [ceil(self.rf[7]/self.dil[7]),1]
+        self.tcb21 = TempConvBlock(
+                ch_in = self.ch[6],
+                ch_out = self.ch[7],
+                k_size = k_tcb21,
+                dil = [self.dil[7],1],
+                pad = [((k_tcb21[0]-1)*self.dil[7]+1)//2,0]
+                )
+        # self.padding = nn.ConstantPad2d((0,0,1,1), 0)
+        k_cb2 = [ceil(self.rf[8]/self.dil[8]),1]
+        self.cb2 = ConvBlock(
+                ch_in = self.ch[7],
+                ch_out = self.ch[8],
+                k_size = k_cb2,
+                strd = [4,1],
+                pad = [((k_cb2[0]-1)*self.dil[8]+1)//2, 0],
+                strd_avg  = [2,1]
+                )        
+        # # 1st instance of regressor 
+        self.fc0 = FC(
+                ft_in = self.ch[8] * 5,
+                ft_out = self.ch[9]
+        )
+        
+        # 2nd instance of FC 
+        self.fc1 = nn.Linear(self.ch[9], self.ch[10])
+        # self.out_neuron = nn.Linear(
+        #         in_features = self.ch[10],
+        #         out_features = 1
+        #         )
+
+    def forward(self, x):
+        x = self.cb0(
+                self.tcb01(
+                    self.tcb00(
+                        x
+                        )
+                    )
+                )
+        x = self.cb1(
+                self.tcb11(
+                    self.tcb10(
+                        x
+                        )
+                    )
+                )
+        # x = self.cb2(
+        #         self.tcb21(
+        #             self.tcb20(
+        #                 x
+        #                 )
+        #             )
+        #         )
+        x = self.tcb21(
+        self.tcb20(
+                x
+                )
+        )
+        # x = self.padding(x)       
+        x = self.cb2(x)
+        
+        x = x.flatten(1)
+        x = self.fc0(
+                x
+        )
+        x = self.fc1(
+                x
+        )
+
+        return x
+
+class TempConvBlock(nn.Module):
+    """
+    Temporal Convolutional Block composed of one temporal convolutional layers.
+    The block is composed of :
+    - Conv1d layer
+    - Chomp1d layer
+    - ReLU layer
+    - BatchNorm1d layer
+
+    :param ch_in: Number of input channels
+    :param ch_out: Number of output channels
+    :param k_size: Kernel size
+    :param dil: Amount of dilation
+    :param pad: Amount of padding
+    """
+    def __init__(self, ch_in, ch_out, k_size, dil, pad):
+        super(TempConvBlock, self).__init__()
+        
+        self.tcn0 = nn.Conv2d(
+                in_channels = ch_in,
+                out_channels = ch_out,
+                kernel_size = k_size,
+                dilation = dil,
+                padding = pad
+                )
+        self.bn0 = nn.BatchNorm2d(
+                num_features = ch_out
+                )
+
+        self.relu0 = nn.ReLU()
+
+       
+    def forward(self, x):
+        x = self.relu0(
+                self.bn0(
+                        self.tcn0(x)
+                )
+        )
+        return x
+
+class ConvBlock(nn.Module):
+    """
+    Convolutional Block composed of:
+    - Conv1d layer
+    - AvgPool1d layer
+    - ReLU layer
+    - BatchNorm1d layer
+
+    :param ch_in: Number of input channels
+    :param ch_out: Number of output channels
+    :param k_size: Kernel size
+    :param strd: Amount of stride
+    :param pad: Amount of padding
+    """
+    def __init__(self, ch_in, ch_out, k_size, strd, pad, strd_avg, dilation=1):
+        super(ConvBlock, self).__init__()
+        
+        self.conv0 = nn.Conv2d(
+                in_channels = ch_in,
+                out_channels = ch_out,
+                kernel_size = k_size,
+                stride = strd,
+                dilation = dilation,
+                padding = pad
+                )
+        self.pool0 = nn.AvgPool2d(
+                kernel_size = [2,1],
+                stride = strd_avg,
+                padding = 0
+                )
+        self.bn0 = nn.BatchNorm2d(ch_out)
+        self.relu0 = nn.ReLU()
+
+
+    def forward(self, x):
+        x = self.relu0(
+                self.bn0(
+                        self.pool0(
+                                self.conv0(
+                                        x
+                                )
+                                
+                        )
+                )
+        )
+                
+        return x
+
+class FC(nn.Module):
+    """
+    Regressor block  composed of :
+    - Linear layer
+    - ReLU layer
+    - BatchNorm1d layer
+
+    :param ft_in: Number of input channels
+    :param ft_out: Number of output channels
+    """
+    def __init__(self, ft_in, ft_out):
+        super(FC, self).__init__()
+        self.ft_in = ft_in
+        self.ft_out = ft_out
+            
+        self.fc0 = nn.Linear(
+                in_features = ft_in,
+                out_features = ft_out
+            )
+        self.bn0 = nn.BatchNorm2d(
+                num_features = ft_out
+            )
+        
+        self.relu0 = nn.ReLU()
+        self.drop0 = nn.Dropout()
+
+
+    def forward(self, x):
+        x = self.fc0(x)
+        x = x.view(*x.size(),1,1)
+        x = self.drop0(
+                self.relu0(
+                        self.bn0(
+                                
+                                x
+                                
+                        )
+                )
+        )
+        x = x.squeeze()
+        return x
+class Chomp1d(nn.Module):
+    """
+    Module that perform a chomping operation on the input tensor.
+    It is used to chomp the amount of zero-padding added on the right of the input tensor, this operation is necessary to compute causal convolutions.
+    :param chomp_size: amount of padding 0s to be removed
+    """
+    def __init__(self, chomp_size):
+        super(Chomp1d, self).__init__()
+        self.chomp_size = chomp_size
+
+    def forward(self, x):
+        return x[:, :, :-self.chomp_size].contiguous()
+                
---
+++ gui_semg_acquisition/modules/_tcn_inference.py
@@ -0,0 +1,184 @@
+"""This module contains the controller for SVM inference.
+
+
+Copyright 2023 Mattia Orlandi, Pierangelo Maria Rapa
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from __future__ import annotations
+
+import logging
+from collections import deque
+
+import torch
+from .TEMPONet_gap import TEMPONet_gap
+
+import numpy as np
+from PySide6.QtCore import QObject, QThread, Signal, Slot
+from PySide6.QtWidgets import QFileDialog, QWidget
+from ..main_window import MainWindow
+from scipy import signal
+from collections import deque
+
+class _SVMWorker(QObject):
+    """Worker that performs inference on the data it receives via a Qt signal.
+
+    Parameters
+    ----------
+    sampFreq : int
+        Sampling frequency.
+
+    Attributes
+    ----------
+    _sampFreq : int
+        Sampling frequency.
+    _bufferCount : int
+        Counter for the inference buffer.
+    _queue : deque
+        Buffer for performing inference.
+
+    Class attributes
+    ----------------
+    inferenceSig : Signal
+        Qt signal emitted when inference is performed.
+    """
+
+    inferenceSig = Signal(int)
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._bufferCount = 0
+        self._bufferSize = 2 * 60 * 1000
+        self._queue = deque()
+
+        self._timeElapsed = 0
+        self._tau = 2 * 3600
+
+        self._model = None
+
+    @property
+    def model(self) -> torch.nn.Module:
+        """SVC: Property representing the SVM model."""
+        return self._model
+
+    @model.setter
+    def model(self, model: torch.nn.Module) -> None:
+        self._model = model
+
+    @Slot(np.ndarray)
+    def predict(self, data: np.ndarray) -> None:
+        """This method is called automatically when the associated signal is received,
+        and it performs the inference on the received data.
+
+        Parameters
+        ----------
+        data : ndarray
+            Data to perform inference on.
+        """
+        for samples in data:
+            self._queue.append(samples[1])  # only PPG_SX
+        self._bufferCount += data.shape[0]
+
+        if self._bufferCount >= self._bufferSize:
+            data = np.asarray(self._queue)
+            # Resample to 20Hz
+            for i in [10,5]:
+                data = signal.decimate(data,i)
+
+            # Add time channel
+            timeElapsed = self._timeElapsed + np.arange(len(data)) / 20.0
+            self._timeElapsed += 30 * 20  # increment by 30s
+            timeWeight = 1 - np.exp(-timeElapsed / self._tau)
+            data = np.vstack([data, timeWeight])
+
+            # Add extra dimension for 2D Convs
+            data = np.expand_dims(data, -1)
+
+            # Inference
+            with torch.no_grad():
+                label = self._model(data).item()
+
+            self.inferenceSig.emit(label)
+            logging.info(f"TCNWorker: predicted label {label}.")
+
+            self._bufferCount = 0
+
+            # update buffer
+            self._queue.popleft(30000) #remove the first 30 seconds of data
+
+
+class TCNInferenceController(QObject):
+    """Controller for SVM inference.
+
+    Attributes
+    ----------
+    _svmWorker : _SVMWorker
+        Worker for performing SVM inference.
+    _svmThread : QThread
+        The QThread associated to the SVM worker.
+
+    Class attributes
+    ----------------
+    _dataReadySig : Signal
+        Qt signal emitted when data is received.
+    """
+
+    _dataReadySig = Signal(np.ndarray)
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._modelPath = "model.pth"
+
+        # Create worker and thread
+        self._svmWorker = _SVMWorker()
+        self._svmThread = QThread()
+        self._svmWorker.moveToThread(self._svmThread)
+
+    def subscribe(self, mainWin: MainWindow) -> None:
+        """Subscribe to instance of MainWindow.
+
+        Parameters
+        ----------
+        mainWin : MainWindow
+            Instance of MainWindow.
+        """
+        mainWin.addWidget(self.confWidget)
+        mainWin.startStreamingSig.connect(self._startInference)
+        mainWin.stopStreamingSig.connect(self._stopInference)
+        mainWin.closeSig.connect(self._stopInference)
+        mainWin.dataReadyFltSig.connect(lambda d: self._dataReadySig.emit(d))
+
+    def _startInference(self) -> None:
+        """Start the inference."""
+        
+        logging.info("SVMInferenceController: inference started.")
+
+        self._svmWorker.model = TEMPONet_gap()
+        checkpoint = torch.load(self._modelPath)
+        self._svmWorker.model.load_state_dict(checkpoint['state_dict'])
+        self._svmWorker.model.eval()
+
+        self._dataReadySig.connect(self._svmWorker.predict)
+        self._svmThread.start()
+
+    def _stopInference(self) -> None:
+        """Stop the inference."""
+        if self._svmThread.isRunning():
+            self._dataReadySig.disconnect(self._svmWorker.predict)
+            self._svmThread.quit()
+            self._svmThread.wait()
+
+            logging.info("SVMInferenceController: inference stopped.")
