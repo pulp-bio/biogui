8c8fd38e8a5cb1b78e2e3a7988de2ac074f3d509
nihil21 <mattia.orlandi21@gmail.com>
2024-06-13 15:26:38 +0200

    Basic OpenGL widget
    Signed-off-by: nihil21 <mattia.orlandi21@gmail.com>

--- biogui/ui/ui_signal_plots_widget.py
+++ biogui/ui/ui_signal_plots_widget.py
@@ -8,7 +8,6 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from pyqtgraph import PlotWidget
 from PySide6.QtCore import (
     QCoreApplication,
     QDate,
@@ -41,7 +40,14 @@ from PySide6.QtGui import (
     QRadialGradient,
     QTransform,
 )
-from PySide6.QtWidgets import QApplication, QSizePolicy, QVBoxLayout, QWidget
+from PySide6.QtWidgets import (
+    QApplication,
+    QGraphicsView,
+    QLabel,
+    QSizePolicy,
+    QVBoxLayout,
+    QWidget,
+)
 
 
 class Ui_SignalPlotsWidget(object):
@@ -51,10 +57,20 @@ class Ui_SignalPlotsWidget(object):
         SignalPlotsWidget.resize(400, 300)
         self.verticalLayout = QVBoxLayout(SignalPlotsWidget)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.graphWidget = PlotWidget(SignalPlotsWidget)
-        self.graphWidget.setObjectName("graphWidget")
+        self.sigNameLabel = QLabel(SignalPlotsWidget)
+        self.sigNameLabel.setObjectName("sigNameLabel")
+        font = QFont()
+        font.setPointSize(18)
+        font.setBold(True)
+        self.sigNameLabel.setFont(font)
+        self.sigNameLabel.setAlignment(Qt.AlignCenter)
 
-        self.verticalLayout.addWidget(self.graphWidget)
+        self.verticalLayout.addWidget(self.sigNameLabel)
+
+        self.view = QGraphicsView(SignalPlotsWidget)
+        self.view.setObjectName("view")
+
+        self.verticalLayout.addWidget(self.view)
 
         self.retranslateUi(SignalPlotsWidget)
 
@@ -66,5 +82,6 @@ class Ui_SignalPlotsWidget(object):
         SignalPlotsWidget.setWindowTitle(
             QCoreApplication.translate("SignalPlotsWidget", "Form", None)
         )
+        self.sigNameLabel.setText("")
 
     # retranslateUi
