79c126a2390668af7c5828e440ef1f8dcd955942
nihil21 <mattia.orlandi21@gmail.com>
2024-06-20 13:04:00 +0200

    Minor adjustments to docstring
    Signed-off-by: nihil21 <mattia.orlandi21@gmail.com>

--- biogui/signal_plot.py
+++ biogui/signal_plot.py
@@ -1,20 +1,21 @@
-# """Class for the real-time plot.
+"""
+Class implementing the real-time plot.
 
 
-# Copyright 2024 Mattia Orlandi, Pierangelo Maria Rapa
+Copyright 2024 Mattia Orlandi, Pierangelo Maria Rapa
 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-# https://www.apache.org/licenses/LICENSE-2.0
+https://www.apache.org/licenses/LICENSE-2.0
 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# """
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 from collections import deque
 
@@ -27,7 +28,8 @@ from .ui.ui_signal_plots_widget import Ui_SignalPlotsWidget
 
 
 class SignalPlotWidget(QWidget, Ui_SignalPlotsWidget):
-    """Widget showing the real-time plot of a signal.
+    """
+    Widget showing the real-time plot of a signal.
 
     Parameters
     ----------
@@ -121,7 +123,8 @@ class SignalPlotWidget(QWidget, Ui_SignalPlotsWidget):
 
     @Slot(np.ndarray)
     def addData(self, data: np.ndarray) -> None:
-        """Add the given data to the internal queues.
+        """
+        Add the given data to the internal queues.
 
         Parameters
         ----------
