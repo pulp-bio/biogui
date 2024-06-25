b791bebf9d0e5507b04926932d946ef05a78daed
nihil21 <mattia.orlandi21@gmail.com>
2024-06-25 11:40:03 +0200

    More robust read
    Signed-off-by: nihil21 <mattia.orlandi21@gmail.com>

--- biogui/data_source/_socket_data_source.py
+++ biogui/data_source/_socket_data_source.py
@@ -21,6 +21,7 @@ from __future__ import annotations
 
 import logging
 import socket
+from asyncio import IncompleteReadError
 
 from PySide6.QtGui import QIntValidator
 from PySide6.QtWidgets import QWidget
@@ -167,15 +168,26 @@ class SocketDataSource(DataSource):
 
                 while not self._stopReadingFlag:
                     try:
-                        data = conn.recv(self._packetSize)
+                        data = bytearray(self._packetSize)
+                        pos = 0
+                        while pos < self._packetSize:
+                            nRead = conn.recv_into(memoryview(data)[pos:])
+                            if nRead == 0:
+                                raise IncompleteReadError(
+                                    bytes[data[:pos]], self._packetSize
+                                )
+                            pos += nRead
                     except socket.timeout:
                         self.errorSig.emit("TCP communication failed.")
                         logging.error("DataWorker: TCP communication failed.")
                         return
+                    except IncompleteReadError as e:
+                        logging.error(
+                            f"DataWorker: read only {len(e.partial)} out of {e.expected} bytes."
+                        )
+                        return
 
-                    # Check number of bytes read
-                    if len(data) == self._packetSize:
-                        self.dataReadySig.emit(data)
+                    self.dataReadySig.emit(data)
 
                 # Stop command
                 for c in self._stopSeq:
@@ -184,7 +196,6 @@ class SocketDataSource(DataSource):
                 # Close connection and socket
                 conn.shutdown(socket.SHUT_RDWR)
                 conn.close()
-                # sock.shutdown(socket.SHUT_RDWR)
                 sock.close()
 
                 logging.info("DataWorker: TCP communication stopped.")
