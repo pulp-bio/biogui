e8f36a1bb561c14c9af4ddddbe52e499cfbe983c
nihil21 <mattia.orlandi21@gmail.com>
2024-09-11 10:19:00 +0200

    Better plots

--- biogui/ui/signal_plots_widget_ui.py
+++ biogui/ui/signal_plots_widget_ui.py
@@ -15,7 +15,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QSizePolicy, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QSizePolicy,
+    QVBoxLayout, QWidget)
 
 from pyqtgraph import PlotWidget
 
@@ -31,6 +32,33 @@ class Ui_SignalPlotsWidget(object):
 
         self.verticalLayout.addWidget(self.graphWidget)
 
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label1 = QLabel(SignalPlotsWidget)
+        self.label1.setObjectName(u"label1")
+        self.label1.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+
+        self.horizontalLayout.addWidget(self.label1)
+
+        self.spsLabel = QLabel(SignalPlotsWidget)
+        self.spsLabel.setObjectName(u"spsLabel")
+
+        self.horizontalLayout.addWidget(self.spsLabel)
+
+        self.label2 = QLabel(SignalPlotsWidget)
+        self.label2.setObjectName(u"label2")
+        self.label2.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+
+        self.horizontalLayout.addWidget(self.label2)
+
+        self.timeLabel = QLabel(SignalPlotsWidget)
+        self.timeLabel.setObjectName(u"timeLabel")
+
+        self.horizontalLayout.addWidget(self.timeLabel)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
 
         self.retranslateUi(SignalPlotsWidget)
 
@@ -39,5 +67,9 @@ class Ui_SignalPlotsWidget(object):
 
     def retranslateUi(self, SignalPlotsWidget):
         SignalPlotsWidget.setWindowTitle(QCoreApplication.translate("SignalPlotsWidget", u"Form", None))
+        self.label1.setText(QCoreApplication.translate("SignalPlotsWidget", u"Sampling rate:", None))
+        self.spsLabel.setText(QCoreApplication.translate("SignalPlotsWidget", u"0 sps", None))
+        self.label2.setText(QCoreApplication.translate("SignalPlotsWidget", u"Time:", None))
+        self.timeLabel.setText(QCoreApplication.translate("SignalPlotsWidget", u"0.00 s", None))
     # retranslateUi
 
