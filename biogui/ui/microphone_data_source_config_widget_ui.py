c8108a86e45a47037f4eb769fa4d76199b0d5f75
petreiuss <79929488+petreiuss@users.noreply.github.com>
2025-10-08 12:24:58 +0200

    Rework Microphone data_source; added check on samplig frequency; removed buffer size field

--- biogui/ui/microphone_data_source_config_widget_ui.py
+++ biogui/ui/microphone_data_source_config_widget_ui.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'microphone_data_source_config_widget.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.7.2
+## Created by: Qt User Interface Compiler version 6.9.0
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -15,29 +15,40 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QLineEdit, QSizePolicy,
-    QTextEdit, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QFormLayout, QLabel,
+    QLineEdit, QSizePolicy, QWidget)
 
 class Ui_MicrophoneDataSourceConfigWidget(object):
     def setupUi(self, MicrophoneDataSourceConfigWidget):
         if not MicrophoneDataSourceConfigWidget.objectName():
             MicrophoneDataSourceConfigWidget.setObjectName(u"MicrophoneDataSourceConfigWidget")
-        MicrophoneDataSourceConfigWidget.resize(400, 300)
-        self.audioDeviceComboBox = QComboBox(MicrophoneDataSourceConfigWidget)
+        MicrophoneDataSourceConfigWidget.resize(361, 52)
+        self.widget = QWidget(MicrophoneDataSourceConfigWidget)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(0, 0, 361, 50))
+        self.formLayout = QFormLayout(self.widget)
+        self.formLayout.setObjectName(u"formLayout")
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.label_3 = QLabel(self.widget)
+        self.label_3.setObjectName(u"label_3")
+
+        self.formLayout.setWidget(0, QFormLayout.ItemRole.LabelRole, self.label_3)
+
+        self.audioDeviceComboBox = QComboBox(self.widget)
         self.audioDeviceComboBox.setObjectName(u"audioDeviceComboBox")
-        self.audioDeviceComboBox.setGeometry(QRect(40, 0, 82, 28))
-        self.sampleRateTextField = QLineEdit(MicrophoneDataSourceConfigWidget)
+
+        self.formLayout.setWidget(0, QFormLayout.ItemRole.FieldRole, self.audioDeviceComboBox)
+
+        self.label = QLabel(self.widget)
+        self.label.setObjectName(u"label")
+
+        self.formLayout.setWidget(1, QFormLayout.ItemRole.LabelRole, self.label)
+
+        self.sampleRateTextField = QLineEdit(self.widget)
         self.sampleRateTextField.setObjectName(u"sampleRateTextField")
-        self.sampleRateTextField.setGeometry(QRect(30, 50, 113, 28))
-        self.textEdit = QTextEdit(MicrophoneDataSourceConfigWidget)
-        self.textEdit.setObjectName(u"textEdit")
-        self.textEdit.setGeometry(QRect(180, 40, 151, 41))
-        self.textEdit_2 = QTextEdit(MicrophoneDataSourceConfigWidget)
-        self.textEdit_2.setObjectName(u"textEdit_2")
-        self.textEdit_2.setGeometry(QRect(180, 90, 151, 41))
-        self.packetSizeTextField = QLineEdit(MicrophoneDataSourceConfigWidget)
-        self.packetSizeTextField.setObjectName(u"packetSizeTextField")
-        self.packetSizeTextField.setGeometry(QRect(30, 100, 113, 28))
+
+        self.formLayout.setWidget(1, QFormLayout.ItemRole.FieldRole, self.sampleRateTextField)
+
 
         self.retranslateUi(MicrophoneDataSourceConfigWidget)
 
@@ -46,24 +57,8 @@ class Ui_MicrophoneDataSourceConfigWidget(object):
 
     def retranslateUi(self, MicrophoneDataSourceConfigWidget):
         MicrophoneDataSourceConfigWidget.setWindowTitle(QCoreApplication.translate("MicrophoneDataSourceConfigWidget", u"Form", None))
+        self.label_3.setText(QCoreApplication.translate("MicrophoneDataSourceConfigWidget", u"Input device:", None))
+        self.label.setText(QCoreApplication.translate("MicrophoneDataSourceConfigWidget", u"Sampling frequency:", None))
         self.sampleRateTextField.setText("")
-        self.textEdit.setHtml(QCoreApplication.translate("MicrophoneDataSourceConfigWidget", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"hr { height: 1px; border-width: 0; }\n"
-"li.unchecked::marker { content: \"\\2610\"; }\n"
-"li.checked::marker { content: \"\\2612\"; }\n"
-"</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Input frequency</p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", None))
-        self.textEdit_2.setHtml(QCoreApplication.translate("MicrophoneDataSourceConfigWidget", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"hr { height: 1px; border-width: 0; }\n"
-"li.unchecked::marker { content: \"\\2610\"; }\n"
-"li.checked::marker { content: \"\\2612\"; }\n"
-"</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Input packet size</p></body></html>", None))
-        self.packetSizeTextField.setText("")
     # retranslateUi
 
