dfa5a8c01e2ae6f13f17dd740509d73350de5652
Enzo1603 <96661578+Enzo1603@users.noreply.github.com>
2025-11-06 21:51:50 +0100

    format

--- biogui/ui/biogui_rc.py
+++ biogui/ui/biogui_rc.py
@@ -5879,10 +5879,17 @@ qt_resource_struct = b"\
 \x00\x00\x01\x94A\xa3\xfa;\
 "
 
+
 def qInitResources():
-    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qRegisterResourceData(
+        0x03, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
+
 
 def qCleanupResources():
-    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+    QtCore.qUnregisterResourceData(
+        0x03, qt_resource_struct, qt_resource_name, qt_resource_data
+    )
+
 
 qInitResources()
