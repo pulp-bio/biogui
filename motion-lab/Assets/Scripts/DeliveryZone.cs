668309b853dbf32d24c880b6bff06de2c53921f6
Enzo1603 <96661578+Enzo1603@users.noreply.github.com>
2026-03-13 13:26:21 +0100

    add C# pre commit

--- motion-lab/Assets/Scripts/DeliveryZone.cs
+++ motion-lab/Assets/Scripts/DeliveryZone.cs
@@ -1,3 +1,8 @@
+// Copyright ETH Zurich - University of Bologna 2026
+// Licensed under Apache v2.0 see LICENSE for details.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 using System.Collections.Generic;
 using UnityEngine;
 
