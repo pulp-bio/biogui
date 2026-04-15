e343d4d0506963e680a3c318e8a804076bfb0201
Enzo1603 <96661578+Enzo1603@users.noreply.github.com>
2026-04-15 22:01:58 +0200

    remove baud rate fifeld

--- biogui/ui/ui_serial_data_source_config_widget.py
+++ biogui/ui/ui_serial_data_source_config_widget.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'serial_data_source_config_widget.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.8.1
+## Created by: Qt User Interface Compiler version 6.9.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -16,22 +16,21 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QComboBox, QFormLayout, QHBoxLayout,
-    QLabel, QLineEdit, QPushButton, QSizePolicy,
-    QWidget)
-from . import biogui_rc
+    QLabel, QPushButton, QSizePolicy, QWidget)
+import biogui_rc
 
 class Ui_SerialDataSourceConfigWidget(object):
     def setupUi(self, SerialDataSourceConfigWidget):
         if not SerialDataSourceConfigWidget.objectName():
             SerialDataSourceConfigWidget.setObjectName(u"SerialDataSourceConfigWidget")
-        SerialDataSourceConfigWidget.resize(400, 84)
+        SerialDataSourceConfigWidget.resize(400, 57)
         self.formLayout = QFormLayout(SerialDataSourceConfigWidget)
         self.formLayout.setObjectName(u"formLayout")
         self.label1 = QLabel(SerialDataSourceConfigWidget)
         self.label1.setObjectName(u"label1")
         self.label1.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
 
-        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label1)
+        self.formLayout.setWidget(0, QFormLayout.ItemRole.LabelRole, self.label1)
 
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
@@ -50,21 +49,9 @@ class Ui_SerialDataSourceConfigWidget(object):
         self.horizontalLayout.setStretch(0, 4)
         self.horizontalLayout.setStretch(1, 1)
 
-        self.formLayout.setLayout(0, QFormLayout.FieldRole, self.horizontalLayout)
-
-        self.label2 = QLabel(SerialDataSourceConfigWidget)
-        self.label2.setObjectName(u"label2")
-        self.label2.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
-
-        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.label2)
-
-        self.baudRateTextField = QLineEdit(SerialDataSourceConfigWidget)
-        self.baudRateTextField.setObjectName(u"baudRateTextField")
-
-        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.baudRateTextField)
+        self.formLayout.setLayout(0, QFormLayout.ItemRole.FieldRole, self.horizontalLayout)
 
         QWidget.setTabOrder(self.serialPortsComboBox, self.rescanSerialPortsButton)
-        QWidget.setTabOrder(self.rescanSerialPortsButton, self.baudRateTextField)
 
         self.retranslateUi(SerialDataSourceConfigWidget)
 
@@ -81,6 +68,5 @@ class Ui_SerialDataSourceConfigWidget(object):
         self.rescanSerialPortsButton.setToolTip(QCoreApplication.translate("SerialDataSourceConfigWidget", u"Rescan serial ports", None))
 #endif // QT_CONFIG(tooltip)
         self.rescanSerialPortsButton.setText("")
-        self.label2.setText(QCoreApplication.translate("SerialDataSourceConfigWidget", u"Baud rate:", None))
     # retranslateUi
 
